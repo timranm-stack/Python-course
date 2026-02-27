b6d4ba55e51590f7cc1e13e63f44df6ecad50fb1
timranm-stack <timranm@ikaslagun.org>
2026-02-27 16:43:08 +0100

    Add files via upload

---
+++ Lesson 1/print and comments.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Muhammad.")
+
+# Printing a fun fact about myself
+# Print("I love playing soccer.")
+print("I love playing soccer.")
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:",3+5 )
