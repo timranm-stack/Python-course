80dcbc717ebc9d84ec39fe3af061cf74a00a5148
timranm-stack <timranm@ikaslagun.org>
2026-03-29 17:02:18 +0200

    Add files via upload

---
+++ tupletolist.py
@@ -0,0 +1,8 @@
+# Store your personal details in a tuple
+personal_details = ("Taha", "LastName", 16, 1.75, 60, "Math")
+
+# Convert the tuple to a list
+personal_details_list = list(personal_details)
+
+print("Tuple:", personal_details)
+print("List:", personal_details_list)
