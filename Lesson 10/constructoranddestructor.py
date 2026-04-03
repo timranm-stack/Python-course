c91e52d17d95008ccb288253f2f301dbc910fded
timranm-stack <timranm@ikaslagun.org>
2026-04-03 16:57:49 +0200

    Add files via upload

---
+++ Lesson 10/constructoranddestructor.py
@@ -0,0 +1,12 @@
+class Employee:
+
+    # Initiliazing (Constructor)
+    def __init__(self):
+        print("Employee created.")
+
+    # Deleting (Destructor)
+    def __del__(self):
+        print("Destructor called, Employee deleted.")
+
+obj = Employee()
+del obj
