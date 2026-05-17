f8a5cf525fb0ba471519b568af6c8571f2d5fc50
timranm-stack <timranm@ikaslagun.org>
2026-05-17 13:42:39 +0200

    Add files via upload

---
+++ Lesson 20/bigO(1).py
@@ -0,0 +1,12 @@
+# Program to show Constant Time complexity
+
+def printnumber(n):
+    iteration=0
+    print("The number entered by user is ",n)
+    iteration+=1
+    print("Total iterations done by the code is ", iteration, "\n")
+
+
+printnumber(10)
+printnumber(20)
+print("\n With any ´n´ the time taken by our code won´t change")
