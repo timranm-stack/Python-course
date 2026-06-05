405ba7ada410066f80e9e1ebb4dbcc0d6c0f1edd
timranm-stack <timranm@ikaslagun.org>
2026-06-05 16:52:28 +0200

    Add files via upload

---
+++ Lesson 25/inplimentbitwise.py
@@ -0,0 +1,27 @@
+# program to show case working of different types of  bitwise oprators
+num1 = 10
+num2 = 4
+# Using AND Operator
+print("num1 & num2 =", num1 & num2)
+# using qr operator
+print("\nnum1 | num2 = ", num1 | num2)
+# Using NOT operator
+print("\n ~num1", ~num1)
+#using xor operator
+print("\nnum1 ^ num2 =", num1 ^ num2)
+
+num1=10
+num2=4
+
+# using rigth shift operator on num1
+print("\nnum1 >> 1 =", num1 >> 1)
+# Using Left Shift operator on num2
+print("\nnum2 >> 1 =", num2 >> 1)
+
+num1 = 10
+num2 = 4
+
+# Using Left Shift operator on num1
+print("\nnum1 << 1=", num1 << 1)
+# Using Left Shift operator on num1
+print("\nnum2 << 1=", num2 << 1)
